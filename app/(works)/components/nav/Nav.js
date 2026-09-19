74089f23131c596616baf3c5735367d7f701fc1c
Luca Denhez <lucadenhez@icloud.com>
2026-09-18 23:55:39 -0700

    Fix blob tokens

--- app/(works)/components/nav/Nav.js
+++ app/(works)/components/nav/Nav.js
@@ -4,6 +4,7 @@ import NavClient from "./NavClient";
 export default async function Nav() {
   const navLang = await getTranslations("nav");
 
+  /*
   const mobileNavItems = [
     { label: navLang("works"), path: "/works", outside: false },
     { label: navLang("photography"), path: "/photography", outside: false },
@@ -18,6 +19,22 @@ export default async function Nav() {
   const desktopNavItems = [
     { label: navLang("works"), path: "/works" },
     { label: navLang("photography"), path: "/photography" },
+    { label: navLang("about"), path: "/about" },
+  ];
+  */
+
+  const mobileNavItems = [
+    { label: navLang("works"), path: "/works", outside: false },
+    { label: navLang("about"), path: "/about", outside: false },
+    {
+      label: navLang("resume"),
+      path: "https://drive.google.com/file/d/1tZ5W3mghiMNkUL9GS9WbxTJuT27AfmCn/view?usp=sharing",
+      outside: true,
+    },
+  ];
+
+  const desktopNavItems = [
+    { label: navLang("works"), path: "/works" },
     { label: navLang("about"), path: "/about" },
   ];
 
